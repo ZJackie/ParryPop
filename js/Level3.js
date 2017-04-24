e92a6cc0f4faa28d6ddb05908ef85d098f1cb8b9
StevenLi219 <steven.li@stonybrook.edu>
2017-04-24 18:28:53 -0400

    lv 3 audio

--- js/Level3.js
+++ js/Level3.js
@@ -25,6 +25,7 @@ Level3 = function() {};
 Level3.prototype = {
 
     create: function() {
+        initAudio();
         invulnerability = false;
         //disable right click menu
         game.canvas.oncontextmenu = function(e) {
