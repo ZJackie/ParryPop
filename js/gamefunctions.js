b2fbae503af3c1bf7fbdd539cf85257cebc3eb16
jazzyguo <jazzy.guo@stonybrook.edu>
2017-04-24 18:46:20 -0400

    fixed persephone function

--- js/gamefunctions.js
+++ js/gamefunctions.js
@@ -314,7 +314,7 @@ function handleEnemyMovements() {
             if (game.physics.arcade.distanceToXY(enemy, player.body.x, player.body.y) < 500) {
                 fireEnemyBullet(enemy);
             }
-        } else if (enemy.enemyType == "persephone") {
+        } else if (enemy.enemyType == "persephone" || enemy.enemyType == "tentacles") {
             handlePersephone(enemy);
         } else if (enemy.enemyType == "hades"){
             //handleHades(enemy);
@@ -426,8 +426,7 @@ function spawnTenctales(NumberOfTentacles) {
         tentacles.body.collides(playerCollisionGroup, takeDamage, this);
         tentacles.body.collides(bulletCollisionGroup, killEnemies, this);
         tentacles.body.collides([enemyCollisionGroup, borderCollisionGroup, playerCollisionGroup]);
-
-        tentacles.rate = Math.random() * 0.4 + 0.1
+        tentacles.rate = Math.random() * 0.4 + 0.1;
     }
 }
 
@@ -751,82 +750,85 @@ function endGame(level) {
 }
 
 function handlePersephone(enemy) {
-    enemy.animations.play('persephoneidle');
-    var random = Math.random() * 5
-    if (random < 2) {
-        whale_2.play();
-    }
-    if (enemy.shield == false) {
-        enemy.currentRadius = enemy.currentRadius - enemy.rate;
-        enemy.body.setCircle(enemy.currentRadius);
-        enemy.body.setCollisionGroup(enemyCollisionGroup);
-        var lowerBound = enemy.width / 2 - enemy.width * 0.1 + 5;
-        var upperBound = enemy.width / 2 + enemy.width * 0.1 + 5;
-        if (enemy.currentRadius <= lowerBound) {
-            enemy.currentRadius = enemy.width;
+    if(enemy.enemyType == "persephone"){
+        enemy.animations.play('persephoneidle');
+        var random = Math.random() * 5
+        if (random < 2) {
+            whale_2.play();
         }
+        if (enemy.shield == false) {
+            enemy.currentRadius = enemy.currentRadius - enemy.rate;
+            enemy.body.setCircle(enemy.currentRadius);
+            enemy.body.setCollisionGroup(enemyCollisionGroup);
+            var lowerBound = enemy.width / 2 - enemy.width * 0.1 + 5;
+            var upperBound = enemy.width / 2 + enemy.width * 0.1 + 5;
+            if (enemy.currentRadius <= lowerBound) {
+                enemy.currentRadius = enemy.width;
+            }
 
-        if (enemy.currentRadius >= lowerBound && enemy.currentRadius <= upperBound) {
-            enemy.body.isVulnerable = true;
+            if (enemy.currentRadius >= lowerBound && enemy.currentRadius <= upperBound) {
+                enemy.body.isVulnerable = true;
+            } else {
+                enemy.body.isVulnerable = false;
+            }
         } else {
+            enemy.currentRadius = enemy.width;
             enemy.body.isVulnerable = false;
         }
-    } else {
-        enemy.currentRadius = enemy.width;
-        enemy.body.isVulnerable = false;
-    }
 
-    enemy.body.rotation = game.physics.arcade.angleBetween(enemy, player) + game.math.degToRad(-90);;
-    game.physics.arcade.moveToXY(enemy, player.body.x, player.body.y, 50);
-    if (game.physics.arcade.distanceToXY(enemy, player.body.x, player.body.y) < 200) {
-        if (enemy.ramAttack == false) {
-            currentrotation = enemy.body.rotation;
-            enemy.ramAttack = true;
-        } else {
-            enemy.body.velocity.x = 500 * Math.cos(currentrotation);
-            enemy.body.velocity.y = 500 * Math.sin(currentrotation);
-            angle = game.physics.arcade.angleBetween(enemy, player);
-            var bubblebullet = enemy.bullets.getFirstExists(false);
-            if (bubblebullet) {
-                whale_shoot.play();
-                game.physics.p2.enable(bubblebullet, true);
-                bubblebullet.enemyType = "enemyBullet";
-                bubblebullet.body.fixedRotation = true;
-                bubblebullet.lifespan = 2000;
-                bubblebullet.reset(enemy.x, enemy.y);
-                bubblebullet.rotation = angle + game.math.degToRad(-90);;
-                bubblebullet.body.velocity.x = 400 * Math.cos(angle);
-                bubblebullet.body.velocity.y = 400 * Math.sin(angle);
-                bubblebullet.isVulnerable = true;
-                bubblebullet.body.setCircle(10)
-                bubblebullet.body.setCollisionGroup(enemybulletCollisionGroup);
-                bubblebullet.body.collides([borderCollisionGroup])
-                bubblebullet.body.collides(swordCollisionGroup, parryBullets, this);
-                bubblebullet.body.collides(playerCollisionGroup, takeBulletDamage, this);
-                bubblebullet.body.collides(bulletCollisionGroup, destroyBullets, this);
+        enemy.body.rotation = game.physics.arcade.angleBetween(enemy, player) + game.math.degToRad(-90);;
+        game.physics.arcade.moveToXY(enemy, player.body.x, player.body.y, 50);
+        if (game.physics.arcade.distanceToXY(enemy, player.body.x, player.body.y) < 200) {
+            if (enemy.ramAttack == false) {
+                currentrotation = enemy.body.rotation;
+                enemy.ramAttack = true;
+            } else {
+                enemy.body.velocity.x = 500 * Math.cos(currentrotation);
+                enemy.body.velocity.y = 500 * Math.sin(currentrotation);
+                angle = game.physics.arcade.angleBetween(enemy, player);
+                var bubblebullet = enemy.bullets.getFirstExists(false);
+                if (bubblebullet) {
+                    whale_shoot.play();
+                    game.physics.p2.enable(bubblebullet, true);
+                    bubblebullet.enemyType = "enemyBullet";
+                    bubblebullet.body.fixedRotation = true;
+                    bubblebullet.lifespan = 2000;
+                    bubblebullet.reset(enemy.x, enemy.y);
+                    bubblebullet.rotation = angle + game.math.degToRad(-90);;
+                    bubblebullet.body.velocity.x = 400 * Math.cos(angle);
+                    bubblebullet.body.velocity.y = 400 * Math.sin(angle);
+                    bubblebullet.isVulnerable = true;
+                    bubblebullet.body.setCircle(10)
+                    bubblebullet.body.setCollisionGroup(enemybulletCollisionGroup);
+                    bubblebullet.body.collides([borderCollisionGroup])
+                    bubblebullet.body.collides(swordCollisionGroup, parryBullets, this);
+                    bubblebullet.body.collides(playerCollisionGroup, takeBulletDamage, this);
+                    bubblebullet.body.collides(bulletCollisionGroup, destroyBullets, this);
+                }
             }
+        } else {
+            game.physics.arcade.moveToXY(enemy, player.body.x, player.body.y, 300);
+            enemy.ramAttack = false;
         }
-    } else {
-        game.physics.arcade.moveToXY(enemy, player.body.x, player.body.y, 300);
-        enemy.ramAttack = false;
+        if (enemy.phase1 == false && enemy.health < 15) {
+            spawnTowers(8, 'bubbleTower');
+            enemy.phase1 = true;
+        }
+        if (enemy.phase2 == false && enemy.health < 10) {
+            spawnSlimes(8, 'blueSlime');
+            enemy.phase2 = true;
+        }
+        if (enemy.phase3 == false && enemy.health < 5) {
+            spawnTenctales(6);
+            player.tentaclecount = 6;
+            enemy.shield = true;
+            enemy.phase3 = true;
+        }
+        if (player.tentaclecount == 0) {
+            enemy.shield = false;
+        } 
     }
-    if (enemy.phase1 == false && enemy.health < 15) {
-        spawnTowers(8, 'bubbleTower');
-        enemy.phase1 = true;
-    }
-    if (enemy.phase2 == false && enemy.health < 10) {
-        spawnSlimes(8, 'blueSlime');
-        enemy.phase2 = true;
-    }
-    if (enemy.phase3 == false && enemy.health < 5) {
-        spawnTenctales(6);
-        player.tentaclecount = 6;
-        enemy.shield = true;
-        enemy.phase3 = true;
-    }
-    if (player.tentaclecount == 0) {
-        enemy.shield = false;
-    } else if (enemy.enemyType == "tentacles") {
+    else if (enemy.enemyType == "tentacles") {
         enemy.animations.play('tentacleidle');
         enemy.currentRadius = enemy.currentRadius - enemy.rate;
         enemy.body.setCircle(enemy.currentRadius);
